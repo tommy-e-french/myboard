93f6551ead730952d456242035e137ae986f7d49
Oz Zapata <oz.zapata@gmail.com>
2013-02-12 03:12:26 -0500

    added test java class

---
+++ src/com/myboard/test/TestABC.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.myboard.test;
+
+/**
+ * @author oz
+ *
+ */
+public class TestABC {
+
+	/**
+	 * 
+	 */
+	public TestABC() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
